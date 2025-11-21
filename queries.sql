463b101a949485f2f833c5e809c789be2077abd7
alekseyskr3d-a11y <alekseyskr3d@gmail.com>
2025-11-21 21:20:49 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,8 +8,10 @@ SELECT
     COUNT(sales.sales_id) AS operations,
     FLOOR(SUM(products.price * sales.quantity)) AS income
 FROM sales
-JOIN employees ON sales.sales_person_id = employees.employee_id
-JOIN products ON sales.product_id = products.product_id
+INNER JOIN employees
+    ON sales.sales_person_id = employees.employee_id
+INNER JOIN products
+    ON sales.product_id = products.product_id
 GROUP BY
     employees.employee_id,
     employees.first_name,
@@ -22,8 +24,10 @@ SELECT
     CONCAT(employees.first_name, ' ', employees.last_name) AS seller,
     FLOOR(AVG(products.price * sales.quantity)) AS average_income
 FROM sales
-JOIN employees ON sales.sales_person_id = employees.employee_id
-JOIN products ON sales.product_id = products.product_id
+INNER JOIN employees
+    ON sales.sales_person_id = employees.employee_id
+INNER JOIN products
+    ON sales.product_id = products.product_id
 GROUP BY
     employees.employee_id,
     employees.first_name,
@@ -31,7 +35,8 @@ GROUP BY
 HAVING FLOOR(AVG(products.price * sales.quantity)) < (
     SELECT FLOOR(AVG(sales_inner.quantity * products_inner.price))
     FROM sales AS sales_inner
-    JOIN products AS products_inner ON sales_inner.product_id = products_inner.product_id
+    INNER JOIN products AS products_inner
+        ON sales_inner.product_id = products_inner.product_id
 )
 ORDER BY average_income;
 
@@ -54,8 +59,10 @@ FROM (
         END AS day_of_week,
         FLOOR(SUM(sales.quantity * products.price)) AS income
     FROM sales
-    JOIN employees ON sales.sales_person_id = employees.employee_id
-    JOIN products ON sales.product_id = products.product_id
+    INNER JOIN employees
+        ON sales.sales_person_id = employees.employee_id
+    INNER JOIN products
+        ON sales.product_id = products.product_id
     GROUP BY
         employees.employee_id,
         employees.first_name,
@@ -92,7 +99,8 @@ SELECT
     COUNT(DISTINCT sales.customer_id) AS total_customers,
     FLOOR(SUM(sales.quantity * products.price)) AS income
 FROM sales
-JOIN products ON sales.product_id = products.product_id
+INNER JOIN products
+    ON sales.product_id = products.product_id
 GROUP BY TO_CHAR(sales.sale_date, 'YYYY-MM')
 ORDER BY selling_month;
 
@@ -104,6 +112,7 @@ WITH first_purchases AS (
     FROM sales
     GROUP BY customer_id
 ),
+
 first_purchase_details AS (
     SELECT DISTINCT ON (first_purchases.customer_id)
         first_purchases.customer_id,
@@ -111,16 +120,21 @@ first_purchase_details AS (
         sales.sales_person_id,
         sales.sales_id
     FROM first_purchases
-    JOIN sales ON first_purchases.customer_id = sales.customer_id
+    INNER JOIN sales
+        ON first_purchases.customer_id = sales.customer_id
         AND first_purchases.first_sale_date = sales.sale_date
-    JOIN products ON sales.product_id = products.product_id
+    INNER JOIN products
+        ON sales.product_id = products.product_id
     WHERE products.price = 0
 )
+
 SELECT
     CONCAT(customers.first_name, ' ', customers.last_name) AS customer,
     first_purchase_details.first_sale_date AS sale_date,
     CONCAT(employees.first_name, ' ', employees.last_name) AS seller
 FROM first_purchase_details
-JOIN customers ON first_purchase_details.customer_id = customers.customer_id
-JOIN employees ON first_purchase_details.sales_person_id = employees.employee_id
+INNER JOIN customers
+    ON first_purchase_details.customer_id = customers.customer_id
+INNER JOIN employees
+    ON first_purchase_details.sales_person_id = employees.employee_id
 ORDER BY customers.customer_id;

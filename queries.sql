1ce1d5803e41de2215e11df9c3534cbfcd9eb027
alekseyskr3d-a11y <alekseyskr3d@gmail.com>
2025-11-21 14:51:03 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,7 +10,7 @@ group by e.employee_id , e.first_name , e.last_name
 order by income desc 
 limit 10;
 --продавцы с выручкой ниже средней
-select concat(e.first_name , ' ', e.last_name ) as seller, floor(avg(p.price * s.quantity)) as average_income 
+select concat(e.first_name , ' ', e.last_name ) as seller, floor(avg(p.price * s.quantity)) as income 
 from  sales s 
 JOIN employees e ON s.sales_person_id = e.employee_id
 JOIN products p ON s.product_id = p.product_id
@@ -20,7 +20,7 @@ having floor(avg(p.price * s.quantity)) <  (
     FROM sales s2
     JOIN products p2 ON s2.product_id = p2.product_id
 )
-order by average_income;
+order by income;
 --выручка по дням недели
 SELECT 
     e.first_name || ' ' || e.last_name AS seller,
@@ -87,4 +87,5 @@ FROM first_purchase_details fpd
 JOIN customers c ON fpd.customer_id = c.customer_id
 JOIN employees e ON fpd.sales_person_id = e.employee_id
 ORDER BY c.customer_id;
+
 
